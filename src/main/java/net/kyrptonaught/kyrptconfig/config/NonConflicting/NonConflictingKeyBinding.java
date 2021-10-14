c5e6560e1101ab14c8ab4b81056cb0289b9fcc64
kyrptonaught <anthonybuttillo10@gmail.com>
2021-10-14 16:39:30 -0400

    init

---
+++ src/main/java/net/kyrptonaught/kyrptconfig/config/NonConflicting/NonConflictingKeyBinding.java
@@ -0,0 +1,19 @@
+package net.kyrptonaught.kyrptconfig.config.NonConflicting;
+
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+
+import java.util.function.Consumer;
+
+public class NonConflictingKeyBinding extends KeyBinding {
+    private final Consumer<InputUtil.Key> keySet;
+    public NonConflictingKeyBinding(String name, InputUtil.Type type, int code, String category, Consumer<InputUtil.Key> keySet) {
+        super(name, type, code, category);
+        this.keySet = keySet;
+    }
+
+    public void setBoundKey(InputUtil.Key boundKey) {
+        super.setBoundKey(boundKey);
+        keySet.accept(boundKey);
+    }
+}
