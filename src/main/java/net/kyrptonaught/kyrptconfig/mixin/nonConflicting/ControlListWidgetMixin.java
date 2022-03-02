1f432f20ab74e8e5ee3f7817835cb60b5da9478d
kyrptonaught <anthonybuttillo10@gmail.com>
2022-03-01 20:07:32 -0500

    Make ControlListWidgetMixin optional to prevent conflicts with other mods

--- src/main/java/net/kyrptonaught/kyrptconfig/mixin/nonConflicting/ControlListWidgetMixin.java
+++ src/main/java/net/kyrptonaught/kyrptconfig/mixin/nonConflicting/ControlListWidgetMixin.java
@@ -3,17 +3,21 @@ package net.kyrptonaught.kyrptconfig.mixin.nonConflicting;
 import net.kyrptonaught.kyrptconfig.config.NonConflicting.NonConflictingKeyBinding;
 import net.minecraft.client.gui.screen.option.ControlsListWidget;
 import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.math.MatrixStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(ControlsListWidget.KeyBindingEntry.class)
+@Mixin(value = ControlsListWidget.KeyBindingEntry.class, priority = -1)
 public class ControlListWidgetMixin {
 
-    @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/option/KeyBinding;equals(Lnet/minecraft/client/option/KeyBinding;)Z"))
+    @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/option/KeyBinding;equals(Lnet/minecraft/client/option/KeyBinding;)Z"), require = 0)
     public boolean dontConflict(KeyBinding instance, KeyBinding other) {
         if (instance instanceof NonConflictingKeyBinding || other instanceof NonConflictingKeyBinding)
             return false;
         return instance.equals(other);
+
     }
 }
